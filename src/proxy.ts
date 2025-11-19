cb56a390113feed1c5bebabd3cfd227a1671c471
Marvyn Souza Madeira <marvynmadeira@gmail.com>
2025-11-18 22:39:04 -0300

    commit: new proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,15 @@
-import { type NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname === "/") {
+    const loginUrl = new URL("/login", request.url);
+
+    return NextResponse.redirect(loginUrl);
+  }
+
   return await updateSession(request);
 }
 
